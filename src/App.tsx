9c54fd20312c203b8afa7f3392c8ffd618bafd53
Mehran Sadeghi <mehran.sadeghi@karnameh.ir>
2025-11-28 18:12:08 +0330

    feat: elevation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import Button from "@components/button"
 import Provider from "@components/provider/Provider"
+import styles from './styles.module.css';
 
 function App() {
 
   return (
     <Provider>
       <Button>hello my button</Button>
+
+      <div className={styles.card}>hello how are you</div>
     </Provider>
   )
 }
